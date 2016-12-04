435f7df63b77f4b70dadbb8c8f326efc50e89c48
Andrew Replogle <andrew.replogle@gmail.com>
2016-12-03 22:13:13 -0600

    refactor to use pk

--- polyencode.py
+++ polyencode.py
@@ -2,18 +2,24 @@
 
 import string
 import sys
+import math
 
-preshiftEncode = {'A':1,'B':2,'C':3,'D':4,'E':5}
-preshiftEncode.update({'F':6,'G':7,'H':8,'I':9,'J':10,'K':11})
-preshiftEncode.update({'L':12,'M':13,'N':14,'O':15,'P':16,'Q':17})
-preshiftEncode.update({'R':18,'S':19,'T':20,'U':21,'V':22,'W':23})
-preshiftEncode.update({'X':24,'Y':25,'Z':0})
+# This would be equivalent to hardcoded public key.
+# Ideally this would be passed in as a dict instead of
+# stored within the script.
+# Every letter is assigned a prime.
 
-preshiftDecode = {1:'A',2:'B',3:'C',4:'D',5:'E'}
-preshiftDecode.update({6:'F',7:'G',8:'H',9:'I',10:'J',11:'K'})
-preshiftDecode.update({12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q'})
-preshiftDecode.update({18:'R',19:'S',20:'T',21:'U',22:'V',23:'W'})
-preshiftDecode.update({24:'X',25:'Y',0:'Z'})
+preshiftEncode = {'A':2,'B':3,'C':5,'D':7,'E':11}
+preshiftEncode.update({'F':13,'G':17,'H':19,'I':23,'J':29,'K':31})
+preshiftEncode.update({'L':37,'M':41,'N':43,'O':47,'P':53,'Q':59})
+preshiftEncode.update({'R':61,'S':67,'T':71,'U':73,'V':79,'W':83})
+preshiftEncode.update({'X':89,'Y':97,'Z':101})
+
+# For all intents, wordEncode is the algorithm. This
+# algorithm takes the value of the current letter in
+# preshiftencode and multiples it by the value of the
+# next letter in the array to arrive at a semiprime
+# for the current letter. Rinse and repeat.
 
 def wordEncode(word):
 
@@ -23,34 +29,28 @@ def wordEncode(word):
     for word in wordArray:
         ltrArray = list(word)
         encLtrArray = []
-        if len(ltrArray) < 2:
-            ltrDec = preshiftDecode[abs(0)]
-            encLtrArray.append(ltrDec)
-            encodedWord = ''.join(encLtrArray)
-            encWordArray.append(encodedWord)
-            continue
         ltrpos = 0
         for ltr in ltrArray:
-            if ltr == ltrArray[0]:
+            if len(ltrArray) < 2:
+                curltr = preshiftEncode[ltr]
+                newltr = int(curltr) * 103
+            elif ltr == ltrArray[0]:
                 curltr = preshiftEncode[ltr]
                 nxtltr = preshiftEncode[ltrArray[1]]
-                newltr = int(curltr) - int(nxtltr)
-                ltrDec = preshiftDecode[abs(newltr)]
+                newltr = int(curltr) * int(nxtltr)
             elif ltr == ltrArray[-1]:
                 curltr = preshiftEncode[ltr]
                 fstltr = preshiftEncode[ltrArray[0]]
-                newltr = int(curltr) - int(fstltr)
-                ltrDec = preshiftDecode[abs(newltr)]
+                newltr = int(curltr) * int(fstltr)
             else:
                 arrayPos = ltrpos + 1
                 curltr = preshiftEncode[ltr]
                 nxtltr = preshiftEncode[ltrArray[arrayPos]]
-                newltr = int(curltr) - int(nxtltr)
-                ltrDec = preshiftDecode[abs(newltr)]
-            encLtrArray.append(ltrDec)
+                newltr = int(curltr) * int(nxtltr)
+            encLtrArray.append(newltr)
             ltrpos = ltrpos+1
 
-        encodedWord = ''.join(encLtrArray)
+        encodedWord = ''.join(str(encLtrArray))
         encWordArray.append(encodedWord)
     print encWordArray
 
